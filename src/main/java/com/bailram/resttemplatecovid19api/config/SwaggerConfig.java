4d0f291b8d4936a72aeb864b68764b75ed169581
bailram <>
2021-05-05 10:30:21 +0700

    add swagger and swagger ui for implement auto generated documentation api

---
+++ src/main/java/com/bailram/resttemplatecovid19api/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.bailram.resttemplatecovid19api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket apiDocumentation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.bailram.resttemplatecovid19api.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Rest Template Covid 19 API",
+                "Learning Spring Boot Rest Template using open Covid 19 API and Swagger for documentation API",
+                "v1.0",
+                "Term of Service",
+                new Contact("Dhimas Bayu Ilham R", "github.com/bailram", "dhimasbayu.9f@gmail.com"),
+                "Open Source",
+                "https://github.com/bailram/resttemplatecovid19api",
+                Collections.emptyList()
+        );
+    }
+}
